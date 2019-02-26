1cbc8d58d6b7c14ad1324300879f86d973779b85
Trejerum <46781165+Trejerum@users.noreply.github.com>
2019-02-26 13:40:01 +0100

    Refactor

---
+++ ExamenEDE2ev/src/interfaz/Etiqueta.java
@@ -0,0 +1,16 @@
+package interfaz;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Etiqueta {
+	public JPanel contentPane;
+	public JTextField txNumDepart;
+	public JTextField txNombre;
+	public JTextField txPoblacion;
+	public JLabel lblRespuesta;
+
+	public Etiqueta() {
+	}
+}
